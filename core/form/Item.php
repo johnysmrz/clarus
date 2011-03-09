d10bb881ced305a724d42afdf0f453e4fac79c3c
Jan Smrz <johnysmrz@users.noreply.github.com>
2011-03-09 04:22:05 +0100

    zmeny ve formularich

--- core/form/Item.php
+++ core/form/Item.php
@@ -5,13 +5,33 @@ abstract class form_Item {
     protected $type = NULL;
     protected $name = NULL;
     protected $label = NULL;
+    protected $form = NULL;
     protected $id = NULL;
+    protected $defaultValue = NULL;
+    protected $value = NULL;
 
-    public function __construct($type, $name, $label = NULL) {
-        $this->type = $type;
-        $this->name = $name;
+    final public function __construct($name, $defaultValue = NULL, $label = NULL) {
+        $this->setup();
+        $this->id = $this->name = $name;
         $this->label = $label;
+        $this->value = $defaultValue;
     }
+
+    final public function setForm(form_Form $form) {
+        $this->form = $form;
+        $this->createName();
+        $this->createId();
+    }
+
+    final private function createName() {
+        $this->name = 'form[' . $this->form->getName() . '][' . $this->name . ']';
+    }
+
+    final private function createId() {
+        $this->id = 'form_' . $this->form->getName() . '_' . $this->id;
+    }
+
+    abstract protected function setup();
 
     public function getType() {
         return $this->type;
---
+++ core/form/item/Password.php
@@ -0,0 +1,9 @@
+<?php
+
+class form_item_Password extends form_Item {
+
+    protected function setup() {
+        $this->type = 'password';
+    }
+
+}
