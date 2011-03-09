f1c4c398c16c8556b0d079db9b92dcc66cdd6aa6
jansmrz <jansmrz@localhost>
2011-03-09 07:34:45 +0100

    Formulare - funkcni betaverze - sablony

--- tpl/original/system/form/form.php
+++ tpl/original/system/form/form.php
@@ -1,8 +1,10 @@
 <table>
-<?php foreach ($this->items as $name => $item): ?>
-    <tr>
-        <td><?php echo $item->getLabel() ?></td>
-        <td><?php $item->display() ?></td>
-    </tr>
-<?php endforeach; ?>
+    <form action="<?php echo $this->action ?>" method="<?php echo $this->method ?>">
+        <?php foreach ($this->items as $name => $item): ?>
+            <tr>
+                <td><?php echo $item->getLabel() ?></td>
+                <td><?php $item->display() ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </form>
 </table>
--- tpl/original/system/form/item.php
+++ tpl/original/system/form/item.php
@@ -1,1 +1,1 @@
-<input type="<?php echo $this->type ?>" value="" name="<?php echo $this->name ?>" />
+<input type="<?php echo $this->type ?>" value="<?php echo $this->value ?>" name="<?php echo $this->getHtmlName() ?>" id="<?php echo $this->getHtmlId() ?>" />
