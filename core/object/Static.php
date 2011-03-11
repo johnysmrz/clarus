8870156fc864e73e6e3bf6e8624c8014ccc8cb3e
Jan Smrz <johnysmrz@users.noreply.github.com>
2011-03-11 06:03:34 +0100

    Upraveny pomocne objekty singleton a static

--- core/object/Static.php
+++ core/object/Static.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+ * Pomocna trida pro staticke objekty, osetruje konstruktor
+ * @author Jan Smrz
+ * @package main
+ * @throws LogicException
+ */
 class object_Static {
 
-    final private function  __construct() {
-        throw new LogicException('Static object canot be instantied', 1);
+    final private function __construct() {
+        throw new LogicException(_('Static object canot be instantied'), 1);
     }
 
 }
