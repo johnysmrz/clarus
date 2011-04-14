b500fb2c4479e4d0dd447e575a99d674ee8593b4
Jan Smrz <johnysmrz@users.noreply.github.com>
2011-04-14 16:57:19 +0200

    Vytvorena vlastni slozka pro konfiguracni soubory, konstanta pro slozku a upraveny potrebne soubory

--- init.php
+++ init.php
@@ -4,7 +4,10 @@ error_reporting(E_ALL);
 ini_set('track_errors', true);
 ini_set('html_errors', false);
 
-$ini = parse_ini_file('default.ini', true);
+$ini = parse_ini_file('conf/default.ini', true);
+
+include_once 'external/firebug/FirePHP.class.php';
+include_once 'external/firebug/fb.php';
 
 session_start();
 
@@ -16,6 +19,7 @@ define('PATH_TPL_C', PATH . $ini['enviroment']['tpl_compiled']);
 define('PATH_LOG', $ini['enviroment']['log_path']);
 define('PATH_CACHE', PATH . $ini['enviroment']['cache_path']);
 define('PATH_APP', PATH . $ini['enviroment']['app_path']);
+define('PATH_CONF', PATH . $ini['enviroment']['conf_path']);
 
 define('FORM_PREFIX', $ini['enviroment']['form_prefix']);
 define('URL', $ini['enviroment']['base_url']);
