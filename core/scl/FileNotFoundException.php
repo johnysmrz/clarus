650c18a5b40f4eee3daa8213546a05bae6f1c8c6
Jan Smrz <johnysmrz@users.noreply.github.com>
2011-04-14 17:07:49 +0200

    Drobna zmena v textu filenotfoundexception aby to bylo lepe pochopitelne

--- core/scl/FileNotFoundException.php
+++ core/scl/FileNotFoundException.php
@@ -5,7 +5,7 @@ namespace clarus\scl;
 class FileNotFoundException extends \clarus\Exception {
 
     public function  __construct($filepath) {
-        parent::__construct('File ['.$filepath.']', 1);
+        parent::__construct('File ['.$filepath.'] not found!', 1);
     }
 
 }
