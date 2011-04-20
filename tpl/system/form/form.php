d7099933fb520c84742fd4fc1ce2509b8e104663
Jan Smrz <johnysmrz@users.noreply.github.com>
2011-04-20 16:45:53 +0200

    update sablon backendu

--- tpl/system/form/form.php
+++ tpl/system/form/form.php
@@ -5,6 +5,18 @@
                 <td>{$item>getLabel()}</td>
                 <td>{$item>display()}</td>
             </tr>
+            <?php if (sizeof($item->getFailMessages()) > 0): ?>
+            <tr>
+                <td style="text-align: right"><img src="/img/admin/alert-ico16.png" alt="{_ Alert icon}" /></td>
+                <td>
+                    <ul class="failMessages">
+                    <?php foreach ($item->getFailMessages() as $message): ?>
+                        <li><?php echo $message ?></li>
+                    <?php endforeach; ?>
+                    </ul>
+                </td>
+            </tr>
+            <?php endif; ?>
         <?php endforeach; ?>
     </table>
 </form>
